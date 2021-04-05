6d626c115b438fbb13a18faef1849f6a2ada903f
willisrc <william.r.cronin@gmail.com>
2021-04-05 19:43:03 -0400

    New Bot

---
+++ src/bot.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	connection: { reconnect: true },
+	channels: [ 'will_is' ],
+  identity: {
+		username: process.env.TWITCH_BOT_USERNAME,
+		password: process.env.TWITCH_OAUTH_TOKEN
+	},
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+  // Ignore echoed messages.
+  if(self) return;
+
+  if(message.toLowerCase() === '!hello') {
+    // "@alca, heya!"
+    client.say(channel, `@${tags.username}, heya!`);
+  }
+});
