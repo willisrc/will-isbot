f804f64f0efc3d26b283ccff3d4932443d2fb5b0
willisrc <william.r.cronin@gmail.com>
2021-04-06 19:47:31 -0400

    Check if user is subscriber or mod

--- src/bot.js
+++ src/bot.js
@@ -31,6 +31,7 @@ client.on('message', (channel, tags, message, self) => {
 
 
 function onJoinHandler (channel, tags, self) {
+  console.log('Someone Joined');
   if (tags.subscriber) {
     client.say(channel, `Good to see you @${tags.username}! Thanks again for the money :P`);
   }
