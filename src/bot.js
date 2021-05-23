705f04f32c2be32726d68aa24aa5752821b38c34
willisrc <william.r.cronin@gmail.com>
2021-05-23 10:33:12 -0400

    vote command test

--- src/bot.js
+++ src/bot.js
@@ -58,10 +58,10 @@ function onMessageHandler (channel, userstate, message, self) {
 				console.log(params);
 				console.log(params[0]);
 				if (userstate.username == 'will_is' || userstate.mod == true) {
-					if (params[0] == 'on') {
+					if (params[0].toString() == 'on') {
 						isVoting = true;
 					}
-					if (params[0] == 'off') {
+					if (params[0].toString() == 'off') {
 						isVoting = false;
 					}
 					else {
